c820b8c6a4dc17ea98d75541275489239c7e63bd
Alvari <alvari.jk@gmail.com>
2023-05-07 15:19:20 +1200

    finished task 1, added new classes for jarvis

--- src/main/java/nz/ac/auckland/se281/Morra.java
+++ src/main/java/nz/ac/auckland/se281/Morra.java
@@ -3,12 +3,50 @@ package nz.ac.auckland.se281;
 import nz.ac.auckland.se281.Main.Difficulty;
 
 public class Morra {
+  private int numMatches;
+  private String playerName;
 
-  public Morra() {}
+  public Morra() {
+    this.numMatches = 0;
 
-  public void newGame(Difficulty difficulty, int pointsToWin, String[] options) {}
+  }
 
-  public void play() {}
+  public void newGame(Difficulty difficulty, int pointsToWin, String[] options) {
+    MessageCli.WELCOME_PLAYER.printMessage(options[0]);
+  
+    //add the player name to the game
+    this.playerName = options[0];
+  }
+
+  public void play() {
+    //increment numMatches and print the start round message
+    this.numMatches++;
+    MessageCli.START_ROUND.printMessage(Integer.toString(this.numMatches));
+
+    //next we must get the number of fingers and sum from the user
+    MessageCli.ASK_INPUT.printMessage();
+    String input = Utils.scanner.nextLine();
+
+    //next ensure the input is valid
+    while (!MyUtils.isValidGameInput(input)) {
+
+      //print the error message for invalid input
+      MessageCli.INVALID_INPUT.printMessage();
+
+      //ask for new input
+      input = Utils.scanner.nextLine();
+    }
+
+    //now lets extract the numbers from the arguments provided
+    int fingers = MyUtils.splitString(input)[0];
+    int sum = MyUtils.splitString(input)[1];
+
+    //print the successfully formatted commands
+    MessageCli.PRINT_INFO_HAND.printMessage(this.playerName, Integer.toString(fingers), Integer.toString(sum));
+
+
+
+  }
 
   public void showStats() {}
 }
---
+++ src/main/java/nz/ac/auckland/se281/MyUtils.java
@@ -0,0 +1,69 @@
+package nz.ac.auckland.se281;
+import java.util.Arrays;
+
+//this class contains several helper methods
+
+public class MyUtils {
+
+    
+    public static boolean isValidGameInput(String input) {
+        //this method checks whether the user correctly formatted their inputs for the game
+
+        //first check if the string is empty or null
+        if (input == null || input.length() == 0) {
+            return false;
+        }
+        
+
+
+        //next we must split the string into 2 parts, separated by a space
+        String[] arguments = input.split(" ", 2);
+
+        //check if it was correctly split
+        if (arguments.length != 2) {
+            return false;
+        }
+        
+        //next check if both strings are integers
+        if (!isInteger(arguments[0]) || !isInteger(arguments[1])) {
+            return false;
+        }
+
+        //convert the strings to numbers
+        int fingers = Integer.parseInt(arguments[0]);
+        int sum = Integer.parseInt(arguments[1]);
+
+        //next check if the numbers are in the correct ranges
+        if (fingers <= 5 && fingers >= 0 && sum >= 1 && sum <= 10) {
+            //if so the result must be valid
+            return true;
+        }
+        //otherwise it must be invalid
+        return false;
+    }
+
+    public static boolean isInteger(String s) {
+            
+        //this method checks if a string contains only numbers 1-9
+        if (!(s.matches("[0-9]+"))) {
+            return false;
+        }
+        return true;
+    }
+
+    public static int[] splitString(String s) {
+        //this method splits a string containing 2 numbers into 2 separate numbers
+
+        //first split the string
+        String[] splitString = s.split(" ", 2);
+
+        //separate the two and convert them to integers
+        int a = Integer.parseInt(splitString[0]);
+        int b = Integer.parseInt(splitString[1]);
+
+        int[] array = {a, b};
+
+        //return the result
+        return array;
+    }
+}
