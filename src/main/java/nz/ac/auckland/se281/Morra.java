00f6a7139fb66b65e7877a6ec7f2ed263c044575
Alvari <alvari.jk@gmail.com>
2023-05-14 14:56:54 +1200

    finished task 6

--- src/main/java/nz/ac/auckland/se281/Morra.java
+++ src/main/java/nz/ac/auckland/se281/Morra.java
@@ -9,13 +9,20 @@ public class Morra {
   private Jarvis jarvis;
   private Human human;
   private ArrayList<Integer> fingersHistory = new ArrayList<>();
+  private int pointToWin;
+  private int jarvisWins;
+  private int playerWins;
 
   public Morra() {
     this.numMatches = -1;
+    this.pointToWin = -1;
   }
 
   public void newGame(Difficulty difficulty, int pointsToWin, String[] options) {
     MessageCli.WELCOME_PLAYER.printMessage(options[0]);
+
+    // reset the game
+    reset();
 
     // create the opponent based on difficulty given
     this.jarvis = JarvisFactory.createJarvis(difficulty);
@@ -23,9 +30,8 @@ public class Morra {
     // create the human player
     this.human = new Human(options[0]);
 
-    // reset the number of matches and wipe the history of plays
-    this.numMatches = 0;
-    fingersHistory.clear();
+    // save the points to win
+    this.pointToWin = pointsToWin;
 
   }
 
@@ -46,6 +52,31 @@ public class Morra {
     // print the result
     getResult(human.getName(), jarvisChoice, humanChoice);
 
+    // now check if the game is over
+    if (pointToWin == jarvisWins) {
+      // print the end game message and exit
+      MessageCli.END_GAME.printMessage("Jarvis", Integer.toString(this.numMatches));
+
+      // reset the game
+      reset();
+
+      // then finish the game
+      finishGame();
+      return;
+    }
+
+    // now check if the human won
+    else if (pointToWin == playerWins) {
+      MessageCli.END_GAME.printMessage(human.getName(), Integer.toString(this.numMatches));
+
+      // reset the game
+      reset();
+
+      // then finish the game
+      finishGame();
+      return;
+    }
+
     // add the humans choice to the database
     fingersHistory.add(humanChoice[0]);
 
@@ -54,6 +85,18 @@ public class Morra {
   }
 
   public void showStats() {
+    // first ensure there is a game
+    if (numMatches == -1) {
+      MessageCli.GAME_NOT_STARTED.printMessage();
+      return;
+    }
+    // print the stats for jarvis
+    MessageCli.PRINT_PLAYER_WINS.printMessage("Jarvis", Integer.toString(jarvisWins),
+        Integer.toString(pointToWin - jarvisWins));
+
+    // print the stats for the player
+    MessageCli.PRINT_PLAYER_WINS.printMessage(human.getName(), Integer.toString(playerWins),
+        Integer.toString(pointToWin - playerWins));
 
   }
 
@@ -64,14 +107,25 @@ public class Morra {
     MessageCli.PRINT_INFO_HAND.printMessage(playerName, Integer.toString(human[0]), Integer.toString(human[1]));
     MessageCli.PRINT_INFO_HAND.printMessage("Jarvis", Integer.toString(jarvis[0]), Integer.toString(jarvis[1]));
 
+    // find the actual sum
     int totalSum = jarvis[0] + human[0];
+
     // first check if both players got it right
     if (jarvis[1] == totalSum && human[1] == totalSum) {
       MessageCli.PRINT_OUTCOME_ROUND.printMessage("DRAW");
+
     } else if (jarvis[1] == totalSum) {
       MessageCli.PRINT_OUTCOME_ROUND.printMessage("AI_WINS");
+
+      // then update the stats for jarvis
+      this.jarvisWins++;
+
     } else if (human[1] == totalSum) {
       MessageCli.PRINT_OUTCOME_ROUND.printMessage("HUMAN_WINS");
+
+      // then update the number of player wins
+      this.playerWins++;
+
     } else {
       MessageCli.PRINT_OUTCOME_ROUND.printMessage("DRAW");
     }
@@ -85,4 +139,18 @@ public class Morra {
     return this.numMatches;
   }
 
+  public void reset() {
+
+    // reset the match
+    this.jarvisWins = 0;
+    this.playerWins = 0;
+    this.numMatches = 0;
+    fingersHistory.clear();
+  }
+
+  public void finishGame() {
+    // this method resets the game once a game is finished
+    this.numMatches = -1;
+    this.pointToWin = -1;
+  }
 }
